c964bf6e14ae8a225f56822cf389df6f2e148b24
viveksingh-01 <viveksingh9301@gmail.com>
2022-04-17 17:50:42 +0530

    wip: implement database connection

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"github.com/viveksingh-01/go-fiber-postgres-rest-api/database"
 	"gorm.io/gorm"
 )
 
@@ -12,10 +15,32 @@ type Repository struct {
 	DB *gorm.DB
 }
 
+func (r *Repository) SetupRoutes(app *fiber.App) {}
+
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config := &database.Config{
+		Host: os.Getenv("DB_HOST"),
+		Port: os.Getenv("DB_PORT"),
+		DBName: os.Getenv("DB_NAME"),
+		User: os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		SSLMode: os.Getenv("DB_SSLMODE"),
+	}
+	db, err := database.Connect(config)
+
+	r := Repository {
+		DB: db,
+	}
+
 	app := fiber.New()
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Status(http.StatusOK).SendString("Hello, World!")
+	r.SetupRoutes(app)
+	app.Get("/healthcheck", func(c *fiber.Ctx) error {
+		return c.Status(http.StatusOK).SendString("All good here!")
 	})
 	log.Fatal(app.Listen(":5000"))
 }
